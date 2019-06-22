06cffb6faf64f987be3a50da8d30aad175bbbb3a
lsbyerley <lsbyerley@gmail.com>
2019-06-22 10:45:49 -0400

    remove server dependecy
    - use aws lambda api host

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const games = require('./games')
-const stats = require('./stats')
-
-router.use(games)
-router.use(stats)
-
-module.exports = router
--- server/models/Stats.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const StatsSchema = new mongoose.Schema({  
-  title: String,
-  updated: Date,
-  updatedReadable: String,
-  data: String
-});
-module.exports = mongoose.model('Stats', StatsSchema, 'stats');
